bc842dfbc2789e3e6d9f404892d7a846928938c9
Adam <acdoyle630@gmail.com>
2017-10-31 13:21:15 -0700

    date and time are saved to database

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6*/
+
 const data = ['first', 'second', 'third'];
 
 function getData(index, cb) {
@@ -5,6 +7,79 @@ function getData(index, cb) {
   if (index < 0) { err = new Error('index out of bounds'); }
   return cb(err, data[index]);
 }
+
+new Promise((resolve, reject ) => {
+  getData(0, (err, data) => {
+    if(err){
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
+})
+
+.then(data => {
+  console.log(`first: ${data}`);
+  });
+
+new Promise((resolve, reject ) => {
+  getData(1, (err, data) => {
+    if(err){
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
+})
+
+.then(data => console.log(`second: ${data}`));
+
+new Promise((resolve, reject ) => {
+  getData(2, (err, data) => {
+    if(err){
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
+})
+
+.then(data => console.log(`third: ${data}`));
+
+
+function getDataAsync(index) {
+  return new Promise((resolve, reject) => {
+    if(index < 0) {return reject(newError('index out of bounds'));}
+    return resolve(data[index]);
+  });
+}
+
+getDataAsync(0)
+.then((data) =>{
+  console.log(data);
+});
+
+
+
+
+
+
+
+  // .then((err, data) => {
+  //   console.log(data);
+  //  return getData(1)
+  //  .then((data) =>{
+  //   console.log(data);
+  //   return getData(2)
+  //   .then((data) =>{
+  //     console.log(data);
+  //   });
+  //  });
+  // });
+
+
+
+
 
 // wrap getData in a promise and console log the output
 
